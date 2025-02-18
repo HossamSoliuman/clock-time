940552d0ef55af031d5eb9dfd047a0cff5dd00a2
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-02-18 20:53:42 +0200

    fix 24 hours to be responsive

--- resources/views/front/time/sections/timeIn24.blade.php
+++ resources/views/front/time/sections/timeIn24.blade.php
@@ -1,15 +1,17 @@
 <section class="convertSection py-5">
-    <div class="container">
-        <div class="text-center mb-4 text-white">
+    <div class="container text-center">
+        <div class="mb-4 text-white">
             <h2 class="searchBetweenA">{{ $timezoneName }} time zone now in 24 hours format</h2>
         </div>
         <div class="row justify-content-center text-white">
-            <div style="font-size: 100px" class='d-block d-md-flex justify-content-center align-items-center w-75 m-auto'>
-                <span id="time24Display" data-initial-time="{{ $date['currentTimeWithSeconds'] }}"></span>
+            <div class="d-flex justify-content-center align-items-center w-100">
+                <span id="time24Display" data-initial-time="{{ $date['currentTimeWithSeconds'] }}"
+                    style="font-size: 10vw;"></span>
             </div>
         </div>
     </div>
 </section>
+
 <script>
     function updateTime() {
         const timeDisplay = document.getElementById('time24Display');
@@ -35,11 +37,9 @@
             `${String(initialHours).padStart(2, '0')}:${String(initialMinutes).padStart(2, '0')}:${String(initialSeconds).padStart(2, '0')}`;
 
         timeDisplay.textContent = formattedTime;
-
         timeDisplay.setAttribute('data-initial-time', formattedTime);
     }
 
     setInterval(updateTime, 1000);
-
     updateTime();
 </script>
