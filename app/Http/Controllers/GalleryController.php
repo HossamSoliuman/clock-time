b20f7c58a6ac25b3039d52fbfd6a2f3864f50c20
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-06-01 16:52:04 +0300

    move to controller

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class GalleryController extends Controller
 {
     public function digitalClocks()
     {
-        return view('front.gallery.digital-clocks')
-            ->with([
-                'head' => 'Digital clock collection'
-            ]);
+        $folder = base_path('../public_html/images/gallery/digital-clocks');
+
+        $images = collect(File::exists($folder) ? File::files($folder) : [])
+            ->map(fn($img) => basename($img->getFilename()));
+
+        $count = $images->count();
+
+        if ($count < 16 && $count > 0) {
+            $repeat = ceil(16 / $count);
+            $images = $images->flatMap(fn($img) => array_fill(0, $repeat, $img))->take(16);
+        }
+
+        return view('front.gallery.digital-clocks')->with([
+            'head' => 'Digital clocks',
+            'images' => $images
+        ]);
     }
+
 
     public function analogClocks()
     {
--- resources/views/front/gallery/digital-clocks.blade.php
+++ resources/views/front/gallery/digital-clocks.blade.php
@@ -1,21 +1,5 @@
-@extends('front.time-converters.shared-sections')
-@section('section')
-    @php
-        $images = File::files('images/gallery/digital-clocks');
-        $count = count($images);
-        if ($count < 16) {
-            $repeat = ceil(16 / $count);
-            $images = collect($images)->flatMap(fn($i) => array_fill(0, $repeat, $i))->take(16);
-        }
-    @endphp
-
-    <div class="container py-5">
-        <div class="row">
-            @foreach ($images as $image)
-                <div class="col-md-3 mb-4">
-                    <img src="{{ asset('images/gallery/digital-clocks/' . basename($image)) }}" class="img-fluid rounded">
-                </div>
-            @endforeach
-        </div>
+@foreach ($images as $image)
+    <div class="col-md-3 mb-4">
+        <img src="{{ asset('images/gallery/digital-clocks/' . $image) }}" class="img-fluid rounded">
     </div>
-@endsection
+@endforeach
