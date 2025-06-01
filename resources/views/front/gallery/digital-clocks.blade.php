3ec9378516aa0ac41f0762fa8a21975b65b692c0
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-06-01 17:18:44 +0300

    fix gallery

--- resources/views/front/gallery/digital-clocks.blade.php
+++ resources/views/front/gallery/digital-clocks.blade.php
@@ -21,7 +21,7 @@
         <div class="row">
             @foreach ($images as $image)
                 <div class="col-md-3 mb-4">
-                    <img src="{{ asset('images/1.jpg') }}" class="img-fluid rounded">
+                    <img src="{{ asset('images/d.jpg') }}" class="img-fluid rounded">
                 </div>
             @endforeach
         </div>
