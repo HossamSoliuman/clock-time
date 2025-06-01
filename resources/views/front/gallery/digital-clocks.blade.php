5fc60ab579016fd2da719d29f721a4fd7149dd60
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-06-01 17:15:00 +0300

    add asset

--- resources/views/front/gallery/digital-clocks.blade.php
+++ resources/views/front/gallery/digital-clocks.blade.php
@@ -21,7 +21,7 @@
         <div class="row">
             @foreach ($images as $image)
                 <div class="col-md-3 mb-4">
-                    <img src="{{ asset('images/gallery/digital-clocks/' . $image) }}" class="img-fluid rounded">
+                    <img src="{{ asset('images/gallery/' . $image) }}" class="img-fluid rounded">
                 </div>
             @endforeach
         </div>
