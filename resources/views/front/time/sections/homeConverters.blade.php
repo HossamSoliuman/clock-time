a3a962ee7689fce58cd10a074b536e7b7bcf9a36
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-06-01 17:51:54 +0300

    link converters from home

--- resources/views/front/time/sections/homeConverters.blade.php
+++ resources/views/front/time/sections/homeConverters.blade.php
@@ -13,42 +13,53 @@
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-clock text-white"></i>
                         </div>
-                        <span class="fw-bold">Hours to decimal</span>
+                        <a href="{{ url('/hours-to-decimal') }}" class="text-white text-decoration-none fw-bold">Hours
+                            to decimal</a>
                     </div>
+
                     <div class="col-md-6 mb-3 d-flex align-items-center">
                         <div class="bg-danger rounded-2 d-flex align-items-center justify-content-center me-2"
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-code text-white"></i>
                         </div>
-                        <span class="fw-bold">Epoch time converter</span>
+                        <a href="{{ url('/epoch-unix') }}" class="text-white text-decoration-none fw-bold">Epoch time
+                            converter</a>
                     </div>
+
                     <div class="col-md-6 mb-3 d-flex align-items-center">
                         <div class="bg-danger rounded-2 d-flex align-items-center justify-content-center me-2"
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-hourglass-start text-white"></i>
                         </div>
-                        <span class="fw-bold">Hours to min</span>
+                        <a href="{{ url('/minutes-to-hours') }}" class="text-white text-decoration-none fw-bold">Hours
+                            to min</a>
                     </div>
+
                     <div class="col-md-6 mb-3 d-flex align-items-center">
                         <div class="bg-danger rounded-2 d-flex align-items-center justify-content-center me-2"
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-hourglass-end text-white"></i>
                         </div>
-                        <span class="fw-bold">Minutes to Hours</span>
+                        <a href="{{ url('/minutes-to-hours') }}" class="text-white text-decoration-none fw-bold">Minutes
+                            to Hours</a>
                     </div>
+
                     <div class="col-md-6 mb-3 d-flex align-items-center">
                         <div class="bg-danger rounded-2 d-flex align-items-center justify-content-center me-2"
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-stopwatch text-white"></i>
                         </div>
-                        <span class="fw-bold">Hours to Seconds</span>
+                        <a href="{{ url('/hours-to-decimal') }}" class="text-white text-decoration-none fw-bold">Hours
+                            to Seconds</a>
                     </div>
+
                     <div class="col-md-6 mb-3 d-flex align-items-center">
                         <div class="bg-danger rounded-2 d-flex align-items-center justify-content-center me-2"
                             style="width: 40px; height: 40px;">
                             <i class="fa-solid fa-clock-rotate-left text-white"></i>
                         </div>
-                        <span class="fw-bold">Minute to Seconds</span>
+                        <a href="{{ url('/minutes-to-hours') }}"
+                            class="text-white text-decoration-none fw-bold">Minute to Seconds</a>
                     </div>
                 </div>
             </div>
